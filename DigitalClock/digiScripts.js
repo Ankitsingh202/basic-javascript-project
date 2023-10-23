eaae242d32c871704bc10b18a4f38c14391d9041
ankit <ankitsingharmy639@gmail.com>
2023-10-24 00:29:03 +0530

    timing and setInterval

---
+++ DigitalClock/digiScripts.js
@@ -0,0 +1,15 @@
+// const clock = document.getElementById('clock');
+// // const clock = document.querySelector('#clock')
+
+// setInterval(function(){
+// let date = new Date();
+// clock.innerHTML = date.toLocaleTimeString();
+
+// },1000);
+
+const clock = document.getElementById('clock');
+
+setInterval(function(){
+  let date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
